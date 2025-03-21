517a49cf95e691f96bb32f3d9398b3e7acda1910
Rahul Tripathi <rahultripathidev@gmail.com>
2025-03-21 19:41:48 +0530

    ft: add swap to program

--- program/src/main.rs
+++ program/src/main.rs
@@ -2,15 +2,24 @@
 sp1_zkvm::entrypoint!(main);
 
 use obsidian_lib::{
+    pack_order,
     states::uni_v2,
-    verifier::{MPTVerifier, VerifierInputs, VerifierOutput},
+    swapper::uni_v2_swapper,
+    verifier::{MPTVerifier, VerifierOutput},
+    ObsidianInput,
 };
 
 pub fn main() {
-    let inputs: VerifierInputs = sp1_zkvm::io::read::<VerifierInputs>();
+    let input: ObsidianInput = sp1_zkvm::io::read::<ObsidianInput>();
+    let block_number = input.block_verifier_inputs.header.number;
 
-    let out: VerifierOutput = MPTVerifier::verify_slot(inputs).unwrap();
-    let reserves = uni_v2::UniV2ReservesState::try_from(out.slot_data.clone());
-    println!("{:0x?}", reserves);
-    sp1_zkvm::io::commit_slice(&out.slot_data);
+    let out: VerifierOutput = MPTVerifier::verify_slot(input.block_verifier_inputs).unwrap();
+    let reserves_state: uni_v2::UniV2ReservesState =
+        uni_v2::UniV2ReservesState::try_from(out.slot_data.clone()).unwrap();
+
+    let swap_out = uni_v2_swapper::swap(reserves_state, input.swap_payload);
+
+    let order = pack_order(swap_out, block_number, out.block_hash);
+
+    sp1_zkvm::io::commit_slice(&order);
 }
