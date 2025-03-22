4f4a925978f4e9b1779375ed158fd89d8350397c
Rahul Tripathi <rahultripathidev@gmail.com>
2025-03-22 15:05:06 +0530

    ft: add sig check and better struct

--- contracts/src/ObsidianRouter.sol
+++ contracts/src/ObsidianRouter.sol
@@ -3,7 +3,10 @@ pragma solidity ^0.8.20;
 
 import {ISP1Verifier} from "@sp1-contracts/ISP1Verifier.sol";
 import {IERC20} from "@openzeppelin/token/ERC20/IERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {console} from "forge-std/console.sol";
+import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import {EIP712} from "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
 
 struct ObsidianOrder {
     address seller;
@@ -15,11 +18,19 @@ struct ObsidianOrder {
     address sell_token;
 }
 
-contract ObsidianRouter {
+contract ObsidianRouter is Ownable, EIP712 {
     address public verifier;
     bytes32 public obsidianProgramVKey;
+    bool public verifyBlock;
 
-    // Events
+    using ECDSA for bytes32;
+
+    // ObsidianOrder hash
+    bytes32 private constant ORDER_TYPEHASH =
+        keccak256(
+            "ObsidianOrder(uint256 blockNumber,uint256 sellAmount,address buyToken,address sellToken)"
+        );
+
     event OrderExecuted(
         address indexed buyer,
         address indexed seller,
@@ -31,29 +42,89 @@ contract ObsidianRouter {
         uint256 blockNumber
     );
 
-    constructor(address _verifier, bytes32 _obsidianProgramVKey) {
+    constructor(
+        address _verifier,
+        bytes32 _obsidianProgramVKey,
+        address owner
+    ) Ownable(owner) EIP712("ObsidianRouter", "1") {
         verifier = _verifier;
         obsidianProgramVKey = _obsidianProgramVKey;
     }
 
+    function setVerifyBlock(bool state) public onlyOwner {
+        verifyBlock = state;
+    }
+
+    /**
+     * @dev creates a hash of the order signed by seller
+     * @param blockNumber agreed block number of the order used for pricing oracle
+     * @param sellAmount amount being sold
+     * @param sellToken address of the token being sold
+     * @param buyToken address of the token being bought
+     */
+    function getOrderHash(
+        uint256 blockNumber,
+        uint256 sellAmount,
+        address buyToken,
+        address sellToken
+    ) public view returns (bytes32) {
+        bytes32 structHash = keccak256(
+            abi.encode(
+                ORDER_TYPEHASH,
+                blockNumber,
+                sellAmount,
+                buyToken,
+                sellToken
+            )
+        );
+        return _hashTypedDataV4(structHash);
+    }
+
+    /**
+     * @dev Verifies that the signature is valid for the given order parameters using EIP-712
+     */
+    function verifySignature(
+        ObsidianOrder memory order,
+        bytes memory signature
+    ) internal view returns (bool) {
+        bytes32 orderHash = getOrderHash(
+            order.block_number,
+            order.sold_amount,
+            order.buy_token,
+            order.sell_token
+        );
+
+        address recoveredSigner = ECDSA.recover(orderHash, signature);
+
+        return recoveredSigner == order.seller;
+    }
+
     function solve(
         bytes calldata _publicValues,
-        bytes calldata _proofBytes
+        bytes calldata _proofBytes,
+        bytes calldata _orderSignature
     ) external {
+        ObsidianOrder memory order = abi.decode(_publicValues, (ObsidianOrder));
+
+        require(
+            verifySignature(order, _orderSignature),
+            "invalid order signature"
+        );
+
         ISP1Verifier(verifier).verifyProof(
             obsidianProgramVKey,
             _publicValues,
             _proofBytes
         );
 
-        ObsidianOrder memory order = abi.decode(_publicValues, (ObsidianOrder));
-
-        bytes32 actualBlockHash = blockhash(order.block_number);
-        require(actualBlockHash != bytes32(0), "Block is too old");
-        require(
-            actualBlockHash == order.block_hash,
-            "Block hash does not match"
-        );
+        if (verifyBlock) {
+            bytes32 actualBlockHash = blockhash(order.block_number);
+            require(actualBlockHash != bytes32(0), "block is too old");
+            require(
+                actualBlockHash == order.block_hash,
+                "block hash does not match"
+            );
+        }
 
         require(
             IERC20(order.buy_token).transferFrom(
@@ -61,7 +132,7 @@ contract ObsidianRouter {
                 order.seller,
                 order.bought_amount
             ),
-            "Buy token transfer failed: Check allowance and balances"
+            "buy token transfer failed: Check allowance and balances"
         );
 
         require(
@@ -70,7 +141,7 @@ contract ObsidianRouter {
                 msg.sender,
                 order.sold_amount
             ),
-            "Sell token transfer failed: Check allowance and balances"
+            "sell token transfer failed: Check allowance and balances"
         );
 
         emit OrderExecuted(
--- contracts/test/Obsidian.t.sol
+++ contracts/test/Obsidian.t.sol
@@ -4,135 +4,6 @@ pragma solidity ^0.8.20;
 import "forge-std/Test.sol";
 import "src/ObsidianRouter.sol";
 
-contract MockERC20 {
-    string public name;
-    string public symbol;
-    uint8 private _decimals;
-    uint256 private _totalSupply;
-
-    mapping(address => uint256) private _balances;
-    mapping(address => mapping(address => uint256)) private _allowances;
-
-    event Transfer(address indexed from, address indexed to, uint256 value);
-    event Approval(
-        address indexed owner,
-        address indexed spender,
-        uint256 value
-    );
-
-    constructor(string memory name_, string memory symbol_, uint8 decimals_) {
-        name = name_;
-        symbol = symbol_;
-        _decimals = decimals_;
-    }
-
-    function decimals() public view virtual returns (uint8) {
-        return _decimals;
-    }
-
-    function balanceOf(address account) public view virtual returns (uint256) {
-        return _balances[account];
-    }
-
-    function transfer(
-        address to,
-        uint256 amount
-    ) public virtual returns (bool) {
-        address owner = msg.sender;
-        _transfer(owner, to, amount);
-        return true;
-    }
-
-    function allowance(
-        address owner,
-        address spender
-    ) public view virtual returns (uint256) {
-        return _allowances[owner][spender];
-    }
-
-    function approve(
-        address spender,
-        uint256 amount
-    ) public virtual returns (bool) {
-        address owner = msg.sender;
-        _approve(owner, spender, amount);
-        return true;
-    }
-
-    function transferFrom(
-        address from,
-        address to,
-        uint256 amount
-    ) public virtual returns (bool) {
-        address spender = msg.sender;
-        _spendAllowance(from, spender, amount);
-        _transfer(from, to, amount);
-        return true;
-    }
-
-    function mint(address to, uint256 amount) public {
-        _mint(to, amount);
-    }
-
-    function _transfer(
-        address from,
-        address to,
-        uint256 amount
-    ) internal virtual {
-        require(from != address(0), "ERC20: transfer from the zero address");
-        require(to != address(0), "ERC20: transfer to the zero address");
-
-        uint256 fromBalance = _balances[from];
-        require(
-            fromBalance >= amount,
-            "ERC20: transfer amount exceeds balance"
-        );
-        unchecked {
-            _balances[from] = fromBalance - amount;
-        }
-        _balances[to] += amount;
-
-        emit Transfer(from, to, amount);
-    }
-
-    function _mint(address account, uint256 amount) internal virtual {
-        require(account != address(0), "ERC20: mint to the zero address");
-
-        _totalSupply += amount;
-        _balances[account] += amount;
-        emit Transfer(address(0), account, amount);
-    }
-
-    function _approve(
-        address owner,
-        address spender,
-        uint256 amount
-    ) internal virtual {
-        require(owner != address(0), "ERC20: approve from the zero address");
-        require(spender != address(0), "ERC20: approve to the zero address");
-
-        _allowances[owner][spender] = amount;
-        emit Approval(owner, spender, amount);
-    }
-
-    function _spendAllowance(
-        address owner,
-        address spender,
-        uint256 amount
-    ) internal virtual {
-        uint256 currentAllowance = allowance(owner, spender);
-        if (currentAllowance != type(uint256).max) {
-            require(
-                currentAllowance >= amount,
-                "ERC20: insufficient allowance"
-            );
-            unchecked {
-                _approve(owner, spender, currentAllowance - amount);
-            }
-        }
-    }
-}
-
 contract MockSP1Verifier is ISP1Verifier {
     mapping(bytes32 => mapping(bytes => mapping(bytes => bool)))
         private _verifyResults;
@@ -168,75 +39,48 @@ contract ObsidianRouterTest is Test {
     address public buyer = address(0xBEEF);
     bytes32 public constant PROGRAM_VKEY = bytes32(uint256(0x123));
 
-    function setUp() public {
-        MockERC20 usdc = new MockERC20("USDC", "USDC", 6);
-        MockERC20 dai = new MockERC20("DAI", "DAI", 18);
-
-        vm.etch(USDC, address(usdc).code);
-        vm.etch(DAI, address(dai).code);
-
-        verifier = new MockSP1Verifier();
-        router = new ObsidianRouter(address(verifier), PROGRAM_VKEY);
-
-        MockERC20(DAI).mint(buyer, type(uint256).max);
-        MockERC20(USDC).mint(SELLER, type(uint256).max);
-
-        vm.prank(buyer);
-        MockERC20(DAI).approve(address(router), type(uint256).max);
-
-        vm.prank(SELLER);
-        MockERC20(USDC).approve(address(router), type(uint256).max);
-    }
-
-    // function testSwap() public {
-    //     bytes
-    //         memory publicValues = hex"000000000000000000000000d4f23afeacfc05399e58e122b9a23cd04fa02c3b6e34099d8047d0e104db4c042d22c86e4365b45ceb1455f7ed05ed0654978da80000000000000000000000000000000000000000000000000000000001512d91000000000000000000000000000000000000000000000000456fe2c89d32bbde00000000000000000000000000000000000000000000000000000000004c4b400000000000000000000000006b175474e89094c44da98b954eedeac495271d0f000000000000000000000000a0b86991c6218b36c1d19d4a2e9eb0ce3606eb48";
-
-    //     bytes
-    //         memory proofBytes = hex"11b6a09d13bd9df98d0c96acdba53f7918caae67f2349200524346e30d61172f2bb6d0920d7891f2fc384fe93e5119fbd21960838aedafd182a9773b759fbee68db77ee50e16ec36d50a60c6cb3910ca3f8f8a79a76a41b88fceed1b1d3f5e8863741e412253542deaf43aefb7c8936666477c3d4cbc7668bad24a68e80ae28ad9b5f24617cee90007a243fc5a4e7bba773fb95c762799d03183bbe0d3e78c5c858eaf68139d7fba6134f634023e821a87b5615f2ea634cb43d6fde1a6dc61cfa2aea82624c823e70c2f12eb699088877ac5e8ec599c5d7454c151d36ee9a11a5acbb01616626cd9f0a52d3ca94ee90b49ce05416035b52d91dd146484c2aca350acfb66";
-
-    //     verifier.setVerifyProofResult(
-    //         PROGRAM_VKEY,
-    //         publicValues,
-    //         proofBytes,
-    //         true
-    //     );
-
-    //     vm.prank(buyer);
-    //     router.solve(publicValues, proofBytes);
-    // }
-
     function testMainnetFork() public {
         uint256 mainnetFork = vm.createFork(vm.envString("MAINNET_RPC_URL"));
         vm.selectFork(mainnetFork);
 
         bytes
-            memory publicValues = hex"000000000000000000000000d4f23afeacfc05399e58e122b9a23cd04fa02c3b8737e5f9d55e8d3bcba36b586dcef5ba6a032f108d6f99e04f446982d05a909e0000000000000000000000000000000000000000000000000000000001513c070000000000000000000000000000000000000000000000004553022b078fae8c00000000000000000000000000000000000000000000000000000000004c4b400000000000000000000000006b175474e89094c44da98b954eedeac495271d0f000000000000000000000000a0b86991c6218b36c1d19d4a2e9eb0ce3606eb48";
+            memory publicValues = hex"000000000000000000000000d4f23afeacfc05399e58e122b9a23cd04fa02c3b4e6d8a6fb809ec5b8d785acadd3a04ad542b55363b24d1c3fea5dc87a298f3a90000000000000000000000000000000000000000000000000000000001513ce80000000000000000000000000000000000000000000000004553022b078fae8c00000000000000000000000000000000000000000000000000000000004c4b400000000000000000000000006b175474e89094c44da98b954eedeac495271d0f000000000000000000000000a0b86991c6218b36c1d19d4a2e9eb0ce3606eb48";
         bytes
-            memory proofBytes = hex"11b6a09d1139b18757b0c5cb67153913a3f14601f942c1cd48cb144903b46820231cc0850eb3ae02692d7eb4e04c1047309a9955aa65e833ff218393094bf201a9d6629d18b3083e561207ec1d9507ae828a1816bd598deed135706759aee2875eec525415d47705ad335ffc1bb7ee607460aa0f39095c08686b1b48ac85050d17bc9d1b14acc7c281f090587f691ea3e7a6adc81414938000870af134cb06450f1c19550283dc25ba7c07e8e5287b0a9961630482a16c9f040006bb09c921b83adcf148227b65d25bf6a72f81db949fc22f4bd1b9959c3647636e63fef0c253e06847bf1dc71a1d1efd31bcf9559efaabad30b0ac97850e7dfa135e3447647b8d5942b8";
+            memory proofBytes = hex"11b6a09d14d5ba6b75d0b7c68da3d6a9757a4aee495e6a61ff914cff527f6794c35c84d00ef6b65ccd43ed859f3312806f2b8fa4b8056e83467ceb600af0e0351761d2632ac586f78260a69a5a25dd3f73ded784751100019a3e86f9072dbaa5e5c1edc308296f61ad25c26aefa141ee6bec8db9a98cada26e74a6c4e782cc5f35d185bc27a30de9fa223c175686055a8317c506d4f90c5e0744642dda56653206aa37dd04eb0c553b4c3a8e27f8e0bb28853cdd0b4d8f257e80b2526f823bbc3239e1f501e99ff1840d04ac537a3e3b0f70a4400f91e198bdbb029c4b5acf460e607c531d196f3da9b8a9328e7ef9a77ad3613de0094a60196da3d33350ac6a946818aa";
 
         ObsidianOrder memory order = abi.decode(publicValues, (ObsidianOrder));
 
         bytes32 vkey = hex"0087e1ea649d141c1b62fc8155f7df892a3ef29b9744fe46ded393fab786638b";
 
-        // Option 1: Use real verifier if available on mainnet
-        // address realVerifierAddress = 0x...; // Replace with actual verifier address
-        // bytes32 realProgramVKey = bytes32(0x...); // Replace with actual program key
-        // router = new ObsidianRouter(realVerifierAddress, realProgramVKey);
+        uint256 sellerPrivateKey = vm.envUint("SELLER_PRIVATE_KEY");
+        address sellerAddress = order.seller;
 
-        // Option 2: Continue with mock verifier for testing (what we'll use)
-        //verifier = new MockSP1Verifier();
         router = new ObsidianRouter(
             address(0x397A5f7f3dBd538f23DE225B51f532c34448dA9B),
-            vkey
+            vkey,
+            sellerAddress
         );
-        //verifier.setVerifyProofResult(vkey, PUBLIC_VALUES, PROOF_BYTES, true);
 
-        // Fund our test addresses with tokens
+        address derivedAddress = vm.addr(sellerPrivateKey);
+        require(
+            derivedAddress == sellerAddress,
+            "private key does not match seller address"
+        );
+
+        bytes32 digest = router.getOrderHash(
+            order.block_number,
+            order.sold_amount,
+            order.buy_token,
+            order.sell_token
+        );
+        console.log(order.block_number);
+
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(sellerPrivateKey, digest);
+        bytes memory signature = abi.encodePacked(r, s, v);
+
         deal(DAI, buyer, 1000000e18);
         deal(USDC, order.seller, 500000000e6);
 
-        // Set approvals
         vm.startPrank(buyer);
         IERC20(DAI).approve(address(router), type(uint256).max);
         vm.stopPrank();
@@ -245,36 +89,23 @@ contract ObsidianRouterTest is Test {
         IERC20(USDC).approve(address(router), type(uint256).max);
         vm.stopPrank();
 
-        // Log initial balances
         console.log("========== Initial Balances ==========");
         console.log("Buyer DAI:", IERC20(DAI).balanceOf(buyer));
         console.log("Buyer USDC:", IERC20(USDC).balanceOf(buyer));
         console.log("Seller DAI:", IERC20(DAI).balanceOf(order.seller));
         console.log("Seller USDC:", IERC20(USDC).balanceOf(order.seller));
 
-        // Execute swap
-        vm.prank(buyer);
-        router.solve(publicValues, proofBytes);
+        // vm.prank(sellerAddress);
+        // router.setVerifyBlock(true);
+        // vm.stopPrank();
 
-        // Log final balances
+        vm.prank(buyer);
+        router.solve(publicValues, proofBytes, signature);
+
         console.log("========== Final Balances ==========");
         console.log("Buyer DAI:", IERC20(DAI).balanceOf(buyer));
         console.log("Buyer USDC:", IERC20(USDC).balanceOf(buyer));
         console.log("Seller DAI:", IERC20(DAI).balanceOf(order.seller));
         console.log("Seller USDC:", IERC20(USDC).balanceOf(order.seller));
-
-        // Verify expected token movements
-        assertEq(
-            IERC20(DAI).balanceOf(order.seller),
-            order.bought_amount,
-            "Seller should receive bought_amount of DAI"
-        );
-        assertEq(
-            IERC20(USDC).balanceOf(buyer),
-            order.sold_amount,
-            "Buyer should receive sold_amount of USDC"
-        );
-
-        console.log(" Mainnet fork swap successful!");
     }
 }
